0fd4079fc9481fdc3d130c3524f569583e1d8d77
github_ntran35 <nvtran196&gmail.com>
2021-03-25 18:17:49 -0400

    First commit C# Codes for BankAccount Program

---
+++ MySuperBank/BankAccount.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance {
+            get 
+            {
+                decimal balance = 0;    //initial the return balance by 0
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;  
+            }
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        //this is the constructor for BankAccount
+        public BankAccount(string name, decimal initialBalance) {
+            this.Owner = name;
+            // this.Balance = initialBalance;
+            MakeDeposit(initialBalance, DateTime.Now, "initial deposit");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        //keep track of all transactions of an individual account holder
+        //in this list
+        private List<Transaction> allTransactions = new List<Transaction>(); 
+
+        public void MakeDeposit(decimal amount, DateTime date, string note) {
+            if (amount <= 0) {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note) {
+            if (amount <= 0) {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public string getAccountHistory() {
+            var report = new StringBuilder();
+            report.AppendLine("Date \t \tAmount\t \tNotes" );
+
+            foreach (var item in allTransactions) {
+                report.AppendLine($"{item.Date.ToShortDateString()} \t{item.Amount} \t{item.Notes}");
+            }
+
+            return report.ToString();
+        }
+
+
+    }
+}
---
+++ MySuperBank/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Nhat", 10000);
+            
+            Console.WriteLine($"Account #{account.Number} was created for {account.Owner} with a 1st deposit of ${account.Balance}");
+
+            //  var account1 = new BankAccount("Quyen", 10000);
+            //  Console.WriteLine($"Account #{account1.Number} was created for {account1.Owner} with a 1st deposit of ${account1.Balance}");
+
+            account.MakeWithdrawal(1000, DateTime.Now, "Piano Purchased");
+            Console.WriteLine($"Current balance after withdrawal is {account.Balance}");
+
+            account.MakeDeposit(300, DateTime.Now, "Paychecks");
+            Console.WriteLine($"Current balance after deposit is {account.Balance}");
+
+            account.MakeWithdrawal(1000, DateTime.Now, "Rent");
+            Console.WriteLine($"Current balance after withdrawal is {account.Balance}");
+
+            account.MakeDeposit(400, DateTime.Now, "Gifts");
+            Console.WriteLine($"Current balance after deposit is {account.Balance}");
+
+            account.MakeWithdrawal(5000, DateTime.Now, "Tuition");
+            Console.WriteLine($"Current balance after withdrawal is {account.Balance}");
+
+            Console.WriteLine($"{account.getAccountHistory()}");
+
+        }
+    }
+}
